a2e6d723aba0ec263fe072377c4a305aeed0e82c
FREAK <freaks.bootcamp@gmail.com>
2024-11-11 00:43:21 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# lesson-01
+# Frames.js Next example
+
+Simple Frames.js app using Next.js.
+
+### Installation
+
+```sh
+npm install
+```
+
+### Run in Debugger
+
+Following command will open frames debugger and also run the app in development mode.
+
+```sh
+npm run dev
+```
---
+++ app/frames/next/route.tsx
@@ -0,0 +1,31 @@
+import { farcasterHubContext } from "frames.js/middleware";
+import { createFrames, Button } from "frames.js/next";
+import { appURL } from "app/utils";
+
+const frames = createFrames({
+  basePath: "/frames",
+  middleware: [
+    farcasterHubContext({
+      // remove if you aren't using @frames.js/debugger or you just don't want to use the debugger hub
+      ...(process.env.NODE_ENV === "production"
+        ? {}
+        : {
+            hubHttpUrl: "http://localhost:3010/hub",
+          }),
+    }),
+  ],
+});
+
+const handleRequest = frames(async (ctx) => {
+  return {
+    image: `${appURL()}/02.png`,
+    buttons: [
+      <Button action="post" target="/">
+        最初のフレームに戻る
+      </Button>,
+    ],
+  };
+});
+
+export const GET = handleRequest;
+export const POST = handleRequest;
